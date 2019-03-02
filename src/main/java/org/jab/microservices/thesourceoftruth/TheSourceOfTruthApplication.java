0c6b37d8f05cb7fc4517276e59b5d38c69e9887d
Juan Antonio Breña Moral <bren@juanantonio.info>
2019-03-02 11:10:22 +0100

    Initial commit

---
+++ src/main/java/org/jab/microservices/thesourceoftruth/TheSourceOfTruthApplication.java
@@ -0,0 +1,13 @@
+package org.jab.microservices.thesourceoftruth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSourceOfTruthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSourceOfTruthApplication.class, args);
+	}
+
+}
