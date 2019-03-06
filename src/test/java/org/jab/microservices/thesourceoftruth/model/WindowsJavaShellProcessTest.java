82be3976010d899361b8d598ac442eb3ad14fbeb
jabrena <bren@juanantonio.info>
2019-03-06 18:38:09 +0100

    Adding minor notes for next tests

--- src/test/java/org/jab/microservices/thesourceoftruth/model/WindowsJavaShellProcessTest.java
+++ src/test/java/org/jab/microservices/thesourceoftruth/model/WindowsJavaShellProcessTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 
 public class WindowsJavaShellProcessTest {
 
+    //git.exe shortlog HEAD -sn --no-merges
+    //git.exe shortlog HEAD -sn --no-merges --since="1 Jan, 2019" --before="1 Feb, 2019""
+    //git.exe log HEAD --since="1 Jan, 2018" --before="1 Feb, 2019" --author="Juan Antonio Brena Moral" --pretty=tformat: --numstat"
+
     @Test
     public void Given_a_Windows_command_When_we_execute_Then_we_receive_the_expected_result() {
 
