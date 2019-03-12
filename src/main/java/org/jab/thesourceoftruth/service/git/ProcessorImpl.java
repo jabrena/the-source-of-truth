97c9fa685f925db4c411bf22cbe080fa82739864
jabrena <bren@juanantonio.info>
2019-03-12 17:19:22 +0100

    Adding git configuration for custom path Adding initial practical execution of Processor

--- src/main/java/org/jab/thesourceoftruth/service/git/ProcessorImpl.java
+++ src/main/java/org/jab/thesourceoftruth/service/git/ProcessorImpl.java
@@ -1,13 +1,26 @@
 package org.jab.thesourceoftruth.service.git;
 
+import lombok.extern.slf4j.Slf4j;
+import org.jab.thesourceoftruth.config.GlobalConfiguration;
 import org.jab.thesourceoftruth.config.Repository;
+import org.jab.thesourceoftruth.service.shell.Command;
+import org.jab.thesourceoftruth.service.shell.Proccess;
+import org.jab.thesourceoftruth.service.shell.ProcessResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ProcessorImpl implements Processor {
+
+    @Autowired
+    private GlobalConfiguration configuration;
+
+    @Autowired
+    private Proccess shellProcess;
 
     final List<Repository> repos;
 
@@ -17,7 +30,16 @@ public class ProcessorImpl implements Processor {
 
     @Override
     public void run() {
-        //repos.stream()
-        //        .forEach();
+
+        repos.stream().forEach(repository -> {
+            ProcessResult result = shellProcess.execute(new Command.Builder()
+                    .add("cd " + repository.getPath())
+                    .add(configuration.getGit() + " shortlog HEAD -sn --no-merges")
+                    .build());
+
+            result.getResults().stream().forEach(System.out::println);
+        });
     }
+
+
 }
--- src/test/java/org/jab/thesourceoftruth/service/git/ProcessorTest.java
+++ src/test/java/org/jab/thesourceoftruth/service/git/ProcessorTest.java
@@ -1,14 +1,25 @@
 package org.jab.thesourceoftruth.service.git;
 
+import org.jab.thesourceoftruth.service.shell.Proccess;
+import org.jab.thesourceoftruth.service.shell.ProcessResult;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.jab.thesourceoftruth.service.shell.WindowsJavaShellProcessTest.getContentAsList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ProcessorTest {
+
+    @MockBean
+    private Proccess shellProcess;
 
     @Autowired
     private Processor processor;
@@ -17,12 +28,14 @@ public class ProcessorTest {
     public void Given_a_configuration_When_call_processor_Then_process_it() throws Exception {
 
         //GIVEN
+        when(shellProcess.execute(any())).thenReturn(
+                new ProcessResult(getContentAsList("git/git-shortlog-sn-no-merges.txt")));
 
         //WHEN
         processor.run();
 
         //THEN
-
+        verify(shellProcess).execute(any());
     }
 
 }
