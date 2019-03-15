7aab153b087b17ceeb4a317b3a942188a502d448
jabrena <bren@juanantonio.info>
2019-03-15 18:32:08 +0100

    Iterating in the history of the repo

--- src/main/java/org/jab/thesourceoftruth/service/git/ProcessorImpl.java
+++ src/main/java/org/jab/thesourceoftruth/service/git/ProcessorImpl.java
@@ -1,5 +1,7 @@
 package org.jab.thesourceoftruth.service.git;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
 import lombok.extern.slf4j.Slf4j;
 import org.jab.thesourceoftruth.config.GlobalConfiguration;
 import org.jab.thesourceoftruth.config.Repository;
@@ -9,7 +11,9 @@ import org.jab.thesourceoftruth.service.shell.ProcessResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
@@ -36,18 +40,39 @@ public class ProcessorImpl implements Processor {
         repos.stream().forEach(repository -> {
 
             //1. Upgrade git branch
-            upgradeBranch(repository);
+            //upgradeBranch(repository);
 
             //2. Get commit ranges (First & Last)
-            getFirstAndLastDate(repository);
+            Tuple2<LocalDate, LocalDate> commitRanges = getFirstAndLastDate(repository);
 
-            //3.
-            getRepoContribution(repository);
+            //3. Get contributions
+            getRepoContribution(repository, commitRanges);
 
         });
     }
 
-    private void getRepoContribution(Repository repository) {
+    private void getRepoContribution(Repository repository, Tuple2<LocalDate, LocalDate> commitRanges) {
+
+        for(int year = commitRanges._1.getYear(); year < commitRanges._2.getYear(); year++) {
+
+            for (int month = 1; month < 12; month++) {
+
+                int currentMonth = month;
+                int nextMonth = month+1;
+
+                String gitDateFilter = "--since=\"1 " + GitMonths.from(currentMonth) +  ", " + year + "\" --before=\"1 " + GitMonths.from(nextMonth) + ", " + year + "\"";
+                System.out.println(gitDateFilter);
+
+                ProcessResult result4 = shellProcess.execute(new Command.Builder()
+                        .add("cd " + repository.getPath())
+                        .add(configuration.getGit() + " shortlog HEAD -sn --no-merges " + gitDateFilter)
+                        .build());
+
+                result4.getResults().stream().forEach(System.out::println);
+            }
+
+        }
+
         LOGGER.info("Get repo contribution metadata");
         ProcessResult result4 = shellProcess.execute(new Command.Builder()
                 .add("cd " + repository.getPath())
@@ -57,7 +82,7 @@ public class ProcessorImpl implements Processor {
         result4.getResults().stream().forEach(System.out::println);
     }
 
-    private void getFirstAndLastDate(Repository repository) {
+    private Tuple2<LocalDate, LocalDate> getFirstAndLastDate(Repository repository) {
         LOGGER.info("Get first & last commit medatadata");
         ProcessResult result2 = shellProcess.execute(new Command.Builder()
                 .add("cd " + repository.getPath())
@@ -80,6 +105,8 @@ public class ProcessorImpl implements Processor {
         }).findFirst().get();
 
         LOGGER.info("{} {}", lastCommit, firstCommit);
+
+        return Tuple.of(firstCommit, lastCommit);
     }
 
     private void upgradeBranch(Repository repository) {
@@ -92,11 +119,11 @@ public class ProcessorImpl implements Processor {
 
         result.getResults().stream().forEach(System.out::println);
 
+        //TODO Create a new method for commands which return errors.
         try {
             ProcessResult result2 = shellProcess.execute(new Command.Builder()
                     .add("cd " + repository.getPath())
                     .add(configuration.getGit() + " checkout master")
-                    //.add(configuration.getGit() + " pull")
                     .build());
 
         } catch (RuntimeException e) {
