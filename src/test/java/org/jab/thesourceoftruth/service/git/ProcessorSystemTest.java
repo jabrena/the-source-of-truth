d61a4c108bd71ce097104b8461b852b203dc7c1d
jabrena <bren@juanantonio.info>
2019-03-20 07:06:17 +0100

    Minor change

--- src/test/java/org/jab/thesourceoftruth/service/git/ProcessorSystemTest.java
+++ src/test/java/org/jab/thesourceoftruth/service/git/ProcessorSystemTest.java
@@ -1,5 +1,6 @@
 package org.jab.thesourceoftruth.service.git;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,7 @@ public class ProcessorSystemTest {
     @Autowired
     private Processor processor;
 
-    //@Disabled
+    @Disabled
     @Test
     public void Given_a_configuration_When_call_processor_Then_process_it() throws Exception {
 
